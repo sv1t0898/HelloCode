b18ff778fb4bf2d3ad26211023ca8257fea87647
dude <dude@dude.ru>
2022-09-18 19:08:23 +0300

    Example010_MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] namearray = {73, 34, 4, 38, 23, 76, 2, 45, 23};
+
+int n = namearray.Length;
+int find = 23;
+
+int index = 0;
+
+while (index < n)
+{
+    if(namearray[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    //index = index + 1;
+    index++;
+}
